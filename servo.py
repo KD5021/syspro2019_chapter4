c2e633c81bf7b95e7282f10a83fc6ce0cbebfd1d
KD5021 <naganokoudai1221@gmail.com>
2019-09-26 04:10:58 +0100

    setservo関数作りました

--- servo.py
+++ servo.py
@@ -1,25 +1,21 @@
 import time
 import RPi.GPIO as GPIO
+import sys
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
 servo = GPIO.PWM(2, 50)
 servo.start(0.0)
 
-bottom = 2.5
-middle = 7.2
-top = 12.0
-
-for i in range(5):
-	servo.ChangeDutyCycle(bottom)
-	time.sleep(1.0)
-
-	servo.ChangeDutyCycle(middle)
-	time.sleep(1.0)
-
-	servo.ChangeDutyCycle(top)
-	time.sleep(1.0)
-
-
-
+def setservo(degree):
+        set=9.5/180*(90+degree)+2.5
+        servo.ChangeDutyCycle(set)
+while 1:
+	deg=input('please input degree=>')
+	if deg>90:
+		break
+	if deg<-90:
+		break
+	setservo(deg)
+GPIO.cleanup()
 
